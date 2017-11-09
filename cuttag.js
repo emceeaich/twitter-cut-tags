2c7b4fba1adeb250cf18fef252c87333dda4c8ff
Emma Humphries <ech@emmah.net>
2017-11-08 19:38:32 -0800

    Update periodically, use shorten text for cut

--- cuttag.js
+++ cuttag.js
@@ -1,16 +1,23 @@
-document.querySelectorAll('.tweet-text')
+const checkTweets = () => {
+    // look for all the tweets we haven't modified
+    document.querySelectorAll('.tweet-text:not(.twitter-cut-tag-added)')
     .forEach(el => {
         var tmp;
         if (el.innerText.length > 140) {
-            el.classList.add('twitter-cut-tag-added');
-            tmp = el.innerHTML;
-            el.innerHTML = '';
-            el.insertAdjacentHTML('afterbegin', 
+            el.classList.add('twitter-cut-tag-added'); // mark as modified
+            tmp = el.innerHTML; // store the tweet
+            el.innerHTML = ''; // clear out the element
+            el.insertAdjacentHTML('afterbegin', // add back with the long tweet behind a cut
             `<details>
-                <summary>Twitter Cut Tag</summary>
+                <summary>${tmp.slice(0,40) + '\u2026'}</summary>
                 <div>${tmp}</div>
             </details>`);
         }
     });
+}
 
-
+/*
+    https://gist.github.com/elgreg/50387f1e850306eaf5ca3d444538c64c
+    check for new tweets
+*/
+window.setInterval(checkTweets, 2000);
